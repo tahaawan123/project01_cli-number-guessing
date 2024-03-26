22d358fa8130e0f85b34b3cb75ae7f363e9767ae
MUHAMMAD TAHA <160233035+tahaawan123@users.noreply.github.com>
2024-03-26 14:31:32 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 8 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please Guess  Number Between 1-8:",
+    },
+]);
+if (answers.userGuessedNumber === randomNumber) {
+    console.log(" Congratulations! You Guessed  right Number");
+}
+else {
+    console.log("You Guessed  Wrong Number");
+}
